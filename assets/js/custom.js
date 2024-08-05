a7e69fa769d7c410c62a04bf43dd79466b3e2a70
Naveed SMR <naveedkhan.smr@gmail.com>
2024-08-05 18:48:10 +0500

    qc new changes done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,4 +3,16 @@ $(document).ready(function () {
         e.preventDefault();
         $($(this).data('bs-target')).toggle(200);
     });
+    $(document).on("click", ".ic", function (e) {
+        e.preventDefault();
+        var index = $(this).index();
+        $(".icons").find(".ic").each(function (i) {
+            $(".icons").find(".ic").eq(i).addClass("far").removeClass("text-golden fas");
+        });
+        $(".icons").find(".ic").each(function (i) {
+            if (i <= index) {
+                $(".icons").find(".ic").eq(i).addClass("text-golden fas").removeClass("far");
+            }
+        });
+    });
 });

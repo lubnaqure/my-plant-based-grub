fedb87f2d9833085a357f75252b9b0dac9265519
Naveed SMR <naveedkhan.smr@gmail.com>
2024-08-05 19:25:32 +0500

    qc new changes done more

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,16 +3,35 @@ $(document).ready(function () {
         e.preventDefault();
         $($(this).data('bs-target')).toggle(200);
     });
+    // $(document).on("click", ".ic", function (e) {
+    //     e.preventDefault();
+    //     var index = $(this).index();
+    //     $(".icons").find(".ic").each(function (i) {
+    //         $(".icons").find(".ic").eq(i).addClass("far").removeClass("text-golden fas");
+    //     });
+    //     $(".icons").find(".ic").each(function (i) {
+    //         if (i <= index) {
+    //             $(".icons").find(".ic").eq(i).addClass("text-golden fas").removeClass("far");
+    //         }
+    //     });
+    // });
     $(document).on("click", ".ic", function (e) {
         e.preventDefault();
         var index = $(this).index();
-        $(".icons").find(".ic").each(function (i) {
-            $(".icons").find(".ic").eq(i).addClass("far").removeClass("text-golden fas");
-        });
-        $(".icons").find(".ic").each(function (i) {
-            if (i <= index) {
-                $(".icons").find(".ic").eq(i).addClass("text-golden fas").removeClass("far");
-            }
-        });
+        var isHighlighted = $(this).hasClass("text-golden");
+
+        if (isHighlighted) {
+            // Remove highlight from all stars
+            $(".icons .ic").removeClass("text-golden fas").addClass("far");
+        } else {
+            // Highlight stars up to the clicked one
+            $(".icons .ic").each(function (i) {
+                if (i <= index) {
+                    $(this).addClass("text-golden fas").removeClass("far");
+                } else {
+                    $(this).removeClass("text-golden fas").addClass("far");
+                }
+            });
+        }
     });
 });
